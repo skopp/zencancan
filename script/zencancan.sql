659d7f7eaeb61b14ac0fb17bcd4aad7e01cb4a70
eric <eric@8c5775ae-2bdc-4a5e-887e-c21f716cea64>
2011-01-17 07:09:07 +0000

    git-svn-id: http://source.zenprog.com/zencancan@13 8c5775ae-2bdc-4a5e-887e-c21f716cea64

---
+++ script/zencancan.sql
@@ -0,0 +1,25 @@
+CREATE TABLE abonnement (
+	`id` varchar(16) NOT NULL,
+	`id_f` int(11) NOT NULL,
+	PRIMARY KEY (`id`,`id_f`),
+	UNIQUE KEY id_f (`id_f`,`id`)
+)  ENGINE=MyISAM  ;
+CREATE TABLE feed (
+	`id_f` int(11) NOT NULL AUTO_INCREMENT,
+	`url` varchar(512) NOT NULL,
+	`title` varchar(255) NOT NULL,
+	`last_id` varchar(255) NOT NULL,
+	`last_maj` datetime NOT NULL,
+	`last_recup` datetime NOT NULL,
+	`link` varchar(255) NOT NULL,
+	`etag` varchar(128) NOT NULL,
+	`last-modified` varchar(128) NOT NULL,
+	`lasterror` varchar(64) NOT NULL,
+	`item_title` varchar(255) NOT NULL,
+	`item_link` varchar(255) NOT NULL,
+	`item_content` text NOT NULL,
+	PRIMARY KEY (`id_f`),
+	UNIQUE KEY url (`url`),
+	UNIQUE KEY last_recup (`last_recup`,`id_f`),
+	KEY last_maj (`last_maj`,`id_f`)
+)  ENGINE=MyISAM  ;
