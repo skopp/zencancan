14a9d782a86f09f660ff1da50788041a0eaa5885
eric <eric@8c5775ae-2bdc-4a5e-887e-c21f716cea64>
2011-02-09 09:36:45 +0000

    git-svn-id: http://source.zenprog.com/zencancan@54 8c5775ae-2bdc-4a5e-887e-c21f716cea64

--- www/site.php
+++ www/site.php
@@ -31,19 +31,21 @@ $pageHTML->menu();
 			<div class="haut"><h2>Description</h2></div>
 			<div class="cont">
 			<p>Avec zenCancan, vous saississez le nom d'un site web (par exemple :<a href='http://www.lemonde.fr/'>Le Monde</a>)
-			et automatiquement, chaque fois que la page du Monde changera, zenCancan 
+			et automatiquement, chaque fois que la page du <em>Monde</em> changera, zenCancan 
 			vous l'indiquera.
 			</p>
 			<br/>
 			
 			<p>zenCancan a été initialement créer pour suivre les flux de commentaires
-			 de billet de blog afin de ne pas polluer les lecteur de flux par de nombreux petits flux.</p>
+			 des billets de blog afin de ne pas polluer les lecteurs de flux par de nombreux petits flux.</p>
 <br/>
-<p>zenCancan sert aussi a suivre des gros sites d'actualité ou des blogs très fréquemment mis
-à jour (par exemple <a href='http://www.presse-citron.net'>Presse Citron</a>) qui noie les autres flux.</p>
+<p>zenCancan sert aussi à suivre des gros sites d'actualité 
+ou des blogs mis
+à jour très fréquemment comme par exemple <a href='http://www.presse-citron.net'>Presse Citron</a>.</p>
 <br/>
-<p>zenCancan est aussi un essai pour que les personnes qui ne savent pas ce qu'est un flux RSS 
-ou une URL puisse suivre simplement des sites web en indiquant juste leur nom.</p>
+<p>zenCancan est aussi une tentative pour que les personnes
+ qui ne savent pas ce qu'est un flux RSS 
+ou une URL puissent suivre simplement des sites web en indiquant juste leur nom.</p>
 			</div>
 			<div class="bas"></div>				
 		</div>		
@@ -54,15 +56,28 @@ ou une URL puisse suivre simplement des sites web en indiquant juste leur nom.</
 				<b>zenCancan</b> offre les fonctionnalités suivantes :
 				<br/>	<br/>
 				<table>
+				
 <tr  class="bgcolor01"><td>Inscription automatique et anonyme </td></tr>
-<tr><td>Ajout d'un flux RSS via : l'url du flux, l'url d'une page HTML disposant d'une alternative RSS, un mot-clé dont
-la recherche via google va afficher une page HTML disposant d'une alternative RSS </td></tr>
-<tr  class="bgcolor01"> <td>Liste des derniers items de l'ensemble des flux configurés trié par date </td></tr>
-<tr><td>Affichage de tous les flux dans un flux RSS unique </td></tr>
-<tr  class="bgcolor01"><td>Liste de tous les items présent sur le flux pour un flux données </td></tr>
+<tr  class=""><td>Inscription avec une URL nommée (eric.zencancan.com) </td></tr>
+<tr  class="bgcolor01"><td>Protection par mot de passe </td></tr>
+<tr><td>Ajout d'un flux RSS via : 
+<ul>
+<li>l'URL du flux,</li>
+ <li>l'URL d'une page HTML disposant d'une alternative RSS,</li>
+<li>un mot-clé dont
+la recherche via <a href='http://www.google.com'>Google</a> 
+va afficher une page HTML disposant d'une alternative RSS</li></ul>
+</td></tr>
+<tr  class="bgcolor01"> <td>Liste des derniers articles de l'ensemble des flux
+ trié par date </td></tr>
+<tr><td>Aggrégation de tous les flux dans un flux RSS unique </td></tr>
+<tr  class="bgcolor01"><td>Liste de tous les items présent sur le flux 
+pour un flux données </td></tr>
 <tr><td>Suppresion de flux </td></tr>
 <tr  class="bgcolor01"><td>Regroupement des flux via un mot-clé (catégorie)</td></tr>
 <tr><td>Import/export des flux en OPML </td></tr>
+<tr  class="bgcolor01"><td>Lecture de l'article directement dans le logiciel</td></tr>
+
 			</table>
 			</div>
 			<div class="bas"></div>				
@@ -71,7 +86,7 @@ la recherche via google va afficher une page HTML disposant d'une alternative RS
 		<div class="box">
 			<div class="haut"><h2>Licence</h2></div>
 			<div class="cont">
-			<a name='licence'/>
+			<a name='licence'></a>
 			<p>zenCancan est un <a href='http://fr.wikipedia.org/wiki/Logiciel_libre'>logiciel libre</a>  diffusé sous les termes de la licence zen :</p>
 			<blockquote>
 			©Sigmalis 2011 - <a href='http://www.sigmalis.com'>Sigmalis</a> vous autorise à utiliser, étudier, copier, modifier et diffuser le logiciel zenCancan 
