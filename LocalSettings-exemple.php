cd4df6097540fa227569e4dba3d6d65ca3d28e45
eric <eric@8c5775ae-2bdc-4a5e-887e-c21f716cea64>
2011-02-16 09:13:37 +0000

    Ajout d'une procédure d'installation
    git-svn-id: http://source.zenprog.com/zencancan@66 8c5775ae-2bdc-4a5e-887e-c21f716cea64

---
+++ LocalSettings-exemple.php
@@ -0,0 +1,13 @@
+<?php
+
+$database_name="zencancan";
+$database_host="localhost";
+$database_login="zencancan";
+$database_password="zencancan";
+
+
+//DOMAINE_NAME est le nom de domaine 
+//du site sur lequel sont installé les
+//comptes anonymes
+//les comptes "nommées" sont alors NOM.DOMAINE_NAME
+define("DOMAIN_NAME","zencancan.local");
