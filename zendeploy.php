8a302a38d15a466eff4583de885f57bffeb2b592
eric <eric@8c5775ae-2bdc-4a5e-887e-c21f716cea64>
2011-02-20 08:34:15 +0000

    git-svn-id: http://source.zenprog.com/zencancan@76 8c5775ae-2bdc-4a5e-887e-c21f716cea64

--- zendeploy.php
+++ zendeploy.php
@@ -1,6 +1,6 @@
 <?php
 
-$project_name = "zenCancan";
+$webapp_name = "zenCancan";
 $licence  = "&copy;Sigmalis 2011 - Sigmalis vous autorise &agrave; utiliser, copier, modifier et diffuser le pr&eacute;sent logiciel sans aucune restriction";
 $description_file = "description.wiki";
 
