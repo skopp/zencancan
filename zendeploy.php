d83bac3a97e609fd55b74b9ccd0e60680a2eca9b
eric <eric@8c5775ae-2bdc-4a5e-887e-c21f716cea64>
2011-04-15 10:33:47 +0000

    Ajout du status, d'un lien vers la description
    git-svn-id: http://source.zenprog.com/zencancan@98 8c5775ae-2bdc-4a5e-887e-c21f716cea64

--- zendeploy.php
+++ zendeploy.php
@@ -4,6 +4,8 @@ $webapp_name = "zenCancan";
 $licence  = "&copy;Sigmalis 2011 - Sigmalis vous autorise &agrave; utiliser, copier, modifier et diffuser le pr&eacute;sent logiciel sans aucune restriction";
 $description_file = "description.wiki";
 $description_short="zenCancan est un lecteur de flux RSS en ligne.";
+$statut = "Production";
+$description = "Voir http://zencancan.com/site.php";
 
 $file_to_preserve = array("LocalSettings.php",'static','script/zencancan');
 $database_definition_file = "script/zencancan.bin";
