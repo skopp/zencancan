01ea4e63f820ad7193dc40eefd4d55acd7423f7e
eric <eric@8c5775ae-2bdc-4a5e-887e-c21f716cea64>
2011-11-01 15:59:42 +0000

    git-svn-id: http://source.zenprog.com/zencancan@118 8c5775ae-2bdc-4a5e-887e-c21f716cea64

--- www/feed.php
+++ www/feed.php
@@ -8,23 +8,3 @@ $id = $recuperateur->get('id');
 $id_f = $recuperateur->getInt('id_f');
 
 $objectInstancier->FeedControler->detailAction($id,$id_f);
-
-
-exit;
-
-
-$pageHTML = new PageHTML($id,$debut,$authentification->getNamedAccount(),$compte->isAdmin($authentification->getId()));
-
-$pageHTML->addRSSURL("Votre flux zencancan","rss.php?id=$id");
-if ($info['tag']){
-	$pageHTML->addRSSURL("Votre flux zencancan - {$info['tag']}","rss.php?id=$id&tag={$info['tag']}");
-}
-$pageHTML->addRSSURL($info['title'],$info['url']);
-
-$pageHTML->haut();
-?>
-
-
-<?php 
-$pageHTML->bas();
-
