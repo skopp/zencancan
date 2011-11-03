074cfc2afd896fd119595caca163d98fc0980e5f
laurent <laurent@8c5775ae-2bdc-4a5e-887e-c21f716cea64>
2011-11-03 20:06:04 +0000

    git-svn-id: http://source.zenprog.com/zencancan@137 8c5775ae-2bdc-4a5e-887e-c21f716cea64

--- template/FluxRead.php
+++ template/FluxRead.php
@@ -1,6 +1,6 @@
 <div id="colonne">
 	<div class="breadcrumbs">
-		<a class='a_btn_nav' href='<?php $this->Path->path() ?>'>&laquo; Revenir a la liste des sites</a>
+		<a class='a_btn_nav' href='<?php $this->Path->path() ?>'>&laquo; Revenir &agrave; la liste des sites</a>
 	</div>
 	<div class="box">
 		<div class="haut"><h2><?php hecho($rssInfo['title']) ?></h2></div>
@@ -34,7 +34,7 @@
 		
 			<div class="box_col">
 				<p class='float_left'>
-				<a href='<?php $this->Path->path("/Feed/detail/$id_f");?>' >« Revenir à la liste des articles</a>
+				<a href='<?php $this->Path->path("/Feed/detail/$id_f");?>' >&laquo; Revenir &agrave; la liste des articles</a>
 				</p>
 				<p class='float_right'>
 				<a href='<?php echo $resultItem['link'] ?>' target='_blank'>Lire l'article original &raquo;</a>
@@ -46,12 +46,12 @@
 			<?php 
 			if ($rejected_tag || $rejected_attributes) : ?>
 				<div>
-					Le document à été purifié de la manière suivantes :
+					Le document &Atilde;� &Atilde;&copy;t&Atilde;&copy; purifi&Atilde;&copy; de la mani&Atilde;&uml;re suivantes :
 					<?php if ($rejected_tag) : ?>
-						<p>Les balises suivantes ont été rejetés : <?php echo implode(",",$rejected_tag)?></p>
+						<p>Les balises suivantes ont &Atilde;&copy;t&Atilde;&copy; rejet&Atilde;&copy;s : <?php echo implode(",",$rejected_tag)?></p>
 					<?php endif;?>
 					<?php if ($rejected_attributes) : ?>
-						<p>Les attributs suivantes ont été rejetés : <?php echo implode(",",$rejected_attributes)?></p>
+						<p>Les attributs suivantes ont &Atilde;&copy;t&Atilde;&copy; rejet&Atilde;&copy;s : <?php echo implode(",",$rejected_attributes)?></p>
 					<?php endif;?>  
 				</div>	
 			<?php endif;?>
