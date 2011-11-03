a2d6b3bdd39c81b843680bc72ed842a938984a04
laurent <laurent@8c5775ae-2bdc-4a5e-887e-c21f716cea64>
2011-11-03 20:17:32 +0000

    git-svn-id: http://source.zenprog.com/zencancan@138 8c5775ae-2bdc-4a5e-887e-c21f716cea64

--- template/Param.php
+++ template/Param.php
@@ -3,7 +3,7 @@
 
 
 <p class="breadcrumbs">
-<a href="<?php $this->Path->path()?>">&laquo; Revenir &agrave; la liste des sites</a>
+<a class="a_btn_nav" href="<?php $this->Path->path()?>">&laquo; Revenir &agrave; la liste des sites</a>
 </p>
 
 
@@ -13,9 +13,9 @@
 <div class="box">
 	<div class="haut"><h2>Modifier un compte</h2></div>
 	<div class="cont">
-		<p>
-		<a href="<?php $this->Path->path("/Param/password")?>">Modifier mon mot de passe</a>
-		</p>
+		<div class="box_deco">
+		<a class='a_btn' href="<?php $this->Path->path("/Param/password")?>">Modifier mon mot de passe</a>
+		</div>
 	</div>
 	<div class="bas"></div>
 </div>
@@ -28,9 +28,9 @@
 
 
 
-	<p class="box_info">
+	<div class="box_deco">
 	R&eacute;cuperer mes donn&eacute;es : <a href='<?php $this->Path->path("/Param/export")?>'>zencancan.opml</a>
-	</p>
+	</div>
 	
 <form class='ff' action='<?php $this->Path->path() ?>' method='post' enctype='multipart/form-data'>
 	<?php $this->Connexion->displayTokenField(); ?>
