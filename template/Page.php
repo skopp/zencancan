c7b7c0f58302d43bda2793b33d53d1bf74b62ee1
laurent <laurent@8c5775ae-2bdc-4a5e-887e-c21f716cea64>
2011-11-07 17:44:29 +0000

    git-svn-id: http://source.zenprog.com/zencancan@148 8c5775ae-2bdc-4a5e-887e-c21f716cea64

--- template/Page.php
+++ template/Page.php
@@ -20,28 +20,31 @@
 					<a href="<?php  $this->Path->path()?>"><img src="<?php $this->Path->echoRessourcePath("/img/commun/logo.png") ?>" alt="" /></a>
 				</div>
 				
-				<?php if ($id_u) : ?>
-				<!-- <div>
-					 <a href='<?php $this->Path->path("/Mur/index")?>'>Mon mur</a>
-					<a href='<?php $this->Path->path("/Feed/list")?>'>Mes sites</a>
-					
-				  </div>  -->	
-				<?php endif;?>
+
 			
 			
 				<div id="menu_login">
-					<?php if ($id_u) : ?>
-						Bienvenue <span class='nom_user'><?php echo $namedAccount ?></span> |
-						<a href='<?php $this->Path->path("/Param/index")?>'>Param&egrave;tres</a> |
-						<a href='<?php $this->Path->path("/Connexion/doLogout")?>'>D&eacute;connexion</a> | 
-						<a href='<?php $this->Path->path("/Aide/presentation")?>'>Aide</a> 
-							<?php if ($isAdmin) : ?>
-								| <a href='<?php $this->Path->path("/Param/admin")?>'>Admin</a> 
-							<?php endif;?>
-					<?php else : ?>
-						<a  href='<?php $this->Path->path('/Connexion/login');?>'>Se connecter</a>
-						<a href='<?php echo $this->Path->getPathWithUsername("",'/Account/create');?>'>Cr&eacute;er un compte</a>
-					<?php endif;?>
+					
+					<div class="item_wall">
+						<?php if ($id_u) : ?>
+							<a href='<?php $this->Path->path("/Mur/index")?>'>Mon mur</a>|
+							<a href='<?php $this->Path->path("/Feed/list")?>'>Mes sites</a>
+						<?php endif;?>
+					</div>
+					<div class="item_login">
+						<?php if ($id_u) : ?>
+							Bienvenue <span class='nom_user'><?php echo $namedAccount ?></span> |
+							<a href='<?php $this->Path->path("/Param/index")?>'>Param&egrave;tres</a> |
+							<a href='<?php $this->Path->path("/Connexion/doLogout")?>'>D&eacute;connexion</a> | 
+							<a href='<?php $this->Path->path("/Aide/presentation")?>'>Aide</a> 
+								<?php if ($isAdmin) : ?>
+									| <a href='<?php $this->Path->path("/Param/admin")?>'>Admin</a> 
+								<?php endif;?>
+						<?php else : ?>
+							<a  href='<?php $this->Path->path('/Connexion/login');?>'>Se connecter</a>
+							<a href='<?php echo $this->Path->getPathWithUsername("",'/Account/create');?>'>Cr&eacute;er un compte</a>
+						<?php endif;?>
+					</div>
 				</div>
 			
 			
