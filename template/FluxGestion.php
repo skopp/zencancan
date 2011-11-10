2e9f73602ee6dd93e92456786ae808a6e174b3f7
laurent <laurent@8c5775ae-2bdc-4a5e-887e-c21f716cea64>
2011-11-10 14:24:51 +0000

    git-svn-id: http://source.zenprog.com/zencancan@168 8c5775ae-2bdc-4a5e-887e-c21f716cea64

--- template/FluxGestion.php
+++ template/FluxGestion.php
@@ -6,29 +6,47 @@
 		Cat&eacute;gorie actuelle : <a href='<?php $this->Path->path("/Feed/list/0/$tag") ?>'><?php hecho($tag) ?></a>
 		<?php endif;?>
 		
+		
 		<form class='ff' action='<?php $this->Path->path() ?>' method='post'>
 			<?php $this->Connexion->displayTokenField(); ?>
 			<input type='hidden' name='path_info' value='/Feed/doAggregate' />
 			<input type='hidden' name='id_f' value='<?php echo $id_f ?>'/>
-			<p>
-			Cat&eacute;gorie : <input type='text' name='tag' value='<?php hecho($tag) ?>' />
+			
+			<p class="align_center">
+			<span>Mettre dans une cat&eacute;gorie</span>
+			<input type='text' name='tag' value='<?php hecho($tag) ?>' />
+			<br/>
+			<input class='a_btn' type='submit' value='Enregistrer'/>
 			</p>
-			<p><input class='submit' type='submit' value='Mettre dans une cat&eacute;gorie'/>	</p>
 		</form>
-		<form action='<?php $this->Path->path() ?>' method='post'>
+		
+		
+		<form class="ff" action='<?php $this->Path->path() ?>' method='post'>
 			<?php $this->Connexion->displayTokenField(); ?>
 			<input type='hidden' name='path_info' value='/Feed/doDelete' />
-			<p>
+			
+			<p class="align_center">
+			<span>D&eacute;sabonnement</span><br/>
 			<input class='submit' type='submit' value='Ne plus suivre'/>
 			</p>
 			<input type='hidden' name='id_f' value='<?php echo $id_f ?>'/>
 		</form>
 		
+		
+		
+		<div class="ff">
+		
 		<?php if ($isAdmin) : ?>
-		<a  class="a_btn" href='<?php $this->Path->path("/Feed/forceReload/$id_f")?>'>Acualiser</a>
+		<a  class="a_btn" href='<?php $this->Path->path("/Feed/forceReload/$id_f")?>'>Actualiser</a>
 		<br/>
 		<?php endif;?>
-		<a  class="a_btn" href='<?php $this->Path->path("/Contact/index/$id_f")?>'>Signaler un problème</a>
+		<p class="align_center">
+		<span>Cette page n'est pas conforme</span><br/>
+		<a class="a_btn" href='<?php $this->Path->path("/Contact/index/$id_f")?>'>Signaler un probl&egrave;me</a>
+		</p>
 		</div>
-		<div class="bas"></div>				
+		
+		
 		</div>
+</div>
+		
