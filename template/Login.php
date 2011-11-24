53631d0d3b0ceef01daed9e314f4d3452986b78a
laurent <laurent@8c5775ae-2bdc-4a5e-887e-c21f716cea64>
2011-11-24 19:21:06 +0000

    git-svn-id: http://source.zenprog.com/zencancan@216 8c5775ae-2bdc-4a5e-887e-c21f716cea64

--- template/Login.php
+++ template/Login.php
@@ -1,40 +1,36 @@
-
-	<?php $this->render("Menu");?>
+<?php $this->render("Menu");?>
 
 
 <div id="contenu">
 
-<div class="box">
-	<div class="haut">
 <?php if ($namedAccount) : ?>
-<h2>Connexion au compte <?php echo $this->Authentification->getFullAccountName(); ?></h2>
+<h1>Connexion au compte <?php echo $this->Authentification->getFullAccountName(); ?></h1>
 <a href='<?php echo $this->Path->getPathWithUsername("","/Connexion/login");?>'>changer de compte</a>
 <?php else : ?>
-<h2>Connexion</h2>
+<h1>Connexion</h1>
 <?php endif;?>
-</div>
-<div class="cont">
+
+
+<div class="box">
 <?php $this->LastMessage->display()?>
 <form class='ff' action='<?php $this->Path->path() ?>' method='post'>
 	<?php $this->Connexion->displayTokenField(); ?>
 	<input type='hidden' name='path_info' value='/Connexion/doLogin' />
 	<?php if (! $namedAccount) : ?>
-		<label for="id_login">Identifiant</label>
-	 	<input type='text' name='login' id="id_login" value='<?php echo $this->LastMessage->getLastInput("login")?>'/>.<?php echo DOMAIN_NAME ?>
-	 	<hr/>
+	
+	<p><label for="id_login">Identifiant</label></p>
+	<p><input type='text' name='login' id="id_login" value='<?php echo $this->LastMessage->getLastInput("login")?>'/>.<?php echo DOMAIN_NAME ?></p>
 	<?php endif; ?>
-		<label for="id_password">Mot de passe</label>
-		<input type='password' id="id_password" name='password'/>
-		<hr/>	
 
-		<p class="align_left">
-		<label for="id_remember">Rester connect&eacute;</label><input type="checkbox" name="remember" id="id_remember" />
-		&nbsp;&nbsp;
-		<input type="submit" class="submit" value="Connexion" />
-		</p>
+	<p><label for="id_password">Mot de passe</label></p>
+	<p><input type='password' id="id_password" name='password'/></p>
+
+	<p><input type="checkbox" name="remember" id="id_remember" /><label for="id_remember">Rester connect&eacute;</label></p>
+
+	<input type="submit" class="submit" value="Connexion" />
+
 </form>
-	</div>
-	<div class="bas"></div>				
+	
 </div>
 
 
